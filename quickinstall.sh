008e06cb3546097a1160b9c776dafbf0275545a6
Ant Kenworthy <mcrmonkey@users.noreply.github.com>
2016-07-15 15:25:47 +0100

    Rejig script
    ~ Moved a few things around - Run all apt stuff first - stuck ufw on the main package line
    ~ Altered the sed run - no need to do the cat stuff
    
    - removed hashes that that nothing after them

--- quickinstall.sh
+++ quickinstall.sh
@@ -1,37 +1,31 @@
-#
+#!/bin/bash
+
+# Upgrade installed packages to latest
+echo -e "\nRunning a package upgrade...\n"
+apt-get -qq -y update && apt-get -qq -y dist-upgrade
+
+
+#Install stuff I use all the time
+echo -e "\nInstalling default packages...\n"
+apt-get -qq -y install build-essential checkinstall docker.io fail2ban git git-core libbz2-dev libc6-dev libgdbm-dev libncursesw5-dev libreadline-gplv2-dev libsqlite3-dev libssl-dev nikto nmap nodejs python-dev python-numpy python-scipy python-setuptools tk-dev unattended-upgrades ufw
+
+
 #Install and configure firewall
-#
-echo -e "\nInstalling and configuring firewall\n"
-apt-get install ufw -y
+echo -e "\nConfiguring firewall...\n"
 ufw default deny incoming
 ufw default allow outgoing
 ufw allow ssh
 
-cat /etc/ufw/ufw.conf | sed 's/ENABLED=no/ENABLED=yes/g' > ~/ufw.conf
-chmod 0644 ~/ufw.conf
-mv -f ~/ufw.conf /etc/ufw/ufw.conf
+sed 's/ENABLED=no/ENABLED=yes/g' /etc/ufw/ufw.conf
 
-#
-# set timezone to Universal Coordinated Time
-#
+# set timezone to UTC
+echo -e "\nUpdating Timezone to UTC...\n"
 sudo timedatectl set-timezone UTC
 
-#
-# Upgrade installed packages to latest
-#
-apt-get update && apt-get dist-upgrade -y
-
-#
-#Install stuff I use all the time
-#
-apt-get install -y build-essential checkinstall docker.io fail2ban git git-core libbz2-dev libc6-dev libgdbm-dev libncursesw5-dev libreadline-gplv2-dev libsqlite3-dev libssl-dev nikto nmap nodejs python-dev python-numpy python-scipy python-setuptools tk-dev unattended-upgrades 
-
-#
 #Install Ruby
-#
+echo -e "\nInstalling Ruby...\n"
 curl -L https://get.rvm.io | bash -s stable --ruby
 
-#
 #PCAP Everything
-#
+echo -e "\nRunning docker: pcap...\n"
 docker run -v ~/pcap:/pcap --net=host -d jgamblin/tcpdump
