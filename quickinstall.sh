b9e0da319428ec68a099c91aad7784cbdb416cb0
Jerry Gamblin <jerry.gamblin@gmail.com>
2016-07-15 17:42:48 -0500

    Update quickinstall.sh
    Fix SED command.

--- quickinstall.sh
+++ quickinstall.sh
@@ -20,7 +20,8 @@ ufw default deny incoming
 ufw default allow outgoing
 ufw allow ssh
 
-sed 's/ENABLED=no/ENABLED=yes/g' /etc/ufw/ufw.conf
+sed -i.bak 's/ENABLED=no/ENABLED=yes/g' /etc/ufw/ufw.conf
+chmod 0644 /etc/ufw/ufw.conf
 
 # set timezone to UTC
 echo -e "\nUpdating Timezone to UTC...\n"
